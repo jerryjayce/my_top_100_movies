69ef7620b1aa0b49d0b075c4be7abf069da4a341
jerryjayce <jerryjayce@gmail.com>
2023-05-14 13:47:10 +0100

    db setup

---
+++ database/mysql/migrations/20230514114517-create-user-movie-list.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_movie_lists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      movie_id: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_movie_lists');
+  }
+};
---
+++ database/mysql/models/user_movie_list.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user_movie_list extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  user_movie_list.init({
+    title: DataTypes.STRING,
+    movie_id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER,
+    rating: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'user_movie_list',
+  });
+  return user_movie_list;
+};
