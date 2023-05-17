84dd02f11098f903263afc637a8c83fda236328b
jerryjayce <jerryjayce@gmail.com>
2023-05-17 06:56:57 +0100

    heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     app.useGlobalPipes(
         new ValidationPipe({ whitelist: true, transform: true }),
     );
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
